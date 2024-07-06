f708115a89cccb63df95ea19dcf8bb6027a8badb
danilkunashev <dan_kunashev@mail.ru>
2024-07-06 23:35:54 +0300

    feat: auth and photo upload v2.1

--- app/server.py
+++ app/server.py
@@ -11,11 +11,18 @@ async def handle_callback(request):
     if not state or not code:
         return web.Response(text="Missing state or code", status=400)
 
-    tg_id = await get_tg_id_by_state(state)
+    try:
+        tg_id = await get_tg_id_by_state(state)
 
-    if await fetch_token(tg_id, state, code):
-        bot = request.app['bot']
-        await bot.send_message(tg_id, "Авторизация прошла успешно!")
+        if await fetch_token(tg_id, state, code):
+            bot = request.app['bot']
+            await bot.send_message(tg_id, "Авторизация прошла успешно!"
+                                          " Загружай свои фотографии с помощью команды /photo")
+
+    except ValueError as e:
+        return web.Response(text=str(e), status=400)
+    except Exception as e:
+        return web.Response(text=f"Error: {str(e)}", status=500)
 
     return web.Response(text="Авторизация прошла успешно, вернитесь в Telegram.")
 

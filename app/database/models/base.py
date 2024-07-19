2bf7f0b1e2067ab1bc1cb1acf25481d3d0f554a5
danilkunashev <dan_kunashev@mail.ru>
2024-07-19 11:29:07 +0300

    fix: better gitignore

--- app/database/models/base.py
+++ app/database/models/base.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import DeclarativeBase
 
 from config import DB_URL
 
-engine = create_async_engine(url=DB_URL)
-async_session = async_sessionmaker(engine)
+engine = create_async_engine(url=DB_URL, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
